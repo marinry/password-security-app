4b7daa6e83b1e89759e261b2f1a71d5f23b7f6a0
Ryan Marin <ram7164@psu.edu>
2026-04-06 17:16:36 -0400

    Clean up server.js

--- server/server.js
+++ server/server.js
@@ -1,40 +1,35 @@
 const express = require('express');
-const cors = require('cors');
-
-const app = express();
-const PORT = 3000;
+const cors    = require('cors');
+const app     = express();
+const PORT    = 3000;
 
 app.use(cors());
 app.use(express.json());
-
-console.log("STATIC FILES SERVED FROM:", __dirname);
-
 app.use(express.static(__dirname));
+
+console.log('Static files served from:', __dirname);
 
 app.post('/analyze', (req, res) => {
     const password = req.body.password || '';
-
     let strength = 'Weak';
     let feedback = [];
 
     if (password.length >= 8) {
         strength = 'Medium';
     }
+
     if (/[A-Z]/.test(password) && /[0-9]/.test(password) && password.length >= 8) {
         strength = 'Strong';
     }
-    if (!/[A-Z]/.test(password)) {
-        feedback.push('Add uppercase letters');
-    }
-    if (!/[0-9]/.test(password)) {
-        feedback.push('Add numbers');
-    }
-    if (password.length < 8) {
-        feedback.push('Use at least 8 characters');
-    }
+
+    if (!/[A-Z]/.test(password))  feedback.push('Add uppercase letters');
+    if (!/[0-9]/.test(password))  feedback.push('Add numbers');
+    if (password.length < 8)      feedback.push('Use at least 8 characters');
+
     if (feedback.length === 0) {
         feedback.push('Strong password');
     }
+
     res.json({ strength, feedback });
 });
 
