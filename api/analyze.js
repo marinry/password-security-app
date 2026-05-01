e32d0bde368a78e190df48fac14b129c1f644e11
Ryan Marin <ram7164@psu.edu>
2026-04-30 22:36:03 -0400

    change syntax for handler

--- api/analyze.js
+++ api/analyze.js
@@ -22,7 +22,7 @@ async function checkHIBP(password) {
     }
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
