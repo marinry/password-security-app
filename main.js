78c9368333b16138e085e0cd912c511a73b6d1b2
Nathan Karp <njk5782@psu.edu>
2026-04-30 22:43:51 -0400

    revert change

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API = 'https://password-security-app-lilac.vercel.app/api/analyze';
+const API = 'https://password-security-app-lilac.vercel.app';
 /* ══════════════════════════════════════════
    TIPS
 ══════════════════════════════════════════ */
